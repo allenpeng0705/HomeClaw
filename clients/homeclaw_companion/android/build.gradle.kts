efc15b549064f311aac161dfd770aefc3cb80219
peng <shileipeng@gmail.com>
2026-02-25 14:26:12 +0800

    update andorid build

--- clients/homeclaw_companion/android/build.gradle.kts
+++ clients/homeclaw_companion/android/build.gradle.kts
@@ -18,6 +18,20 @@ subprojects {
 subprojects {
     project.evaluationDependsOn(":app")
 }
+// Use Java 17 for all subprojects (plugins) to avoid "source value 8 is obsolete" warnings.
+subprojects {
+    afterEvaluate {
+        project.tasks.withType(org.gradle.api.tasks.compile.JavaCompile::class.java).configureEach {
+            sourceCompatibility = "17"
+            targetCompatibility = "17"
+        }
+        project.tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).configureEach {
+            kotlinOptions {
+                jvmTarget = "17"
+            }
+        }
+    }
+}
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
